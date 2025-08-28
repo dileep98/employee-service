1667669d8eca7778ab8fd306a6279eb357ac37b8
dileep98 <dk26092@gmail.com>
2025-08-28 02:25:01 -0400

    RestTemplate config

--- src/main/java/com/dk/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/dk/employeeservice/controller/EmployeeController.java
@@ -2,12 +2,21 @@ package com.dk.employeeservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class EmployeeController {
 
+    private final RestTemplate restTemplate;
+
+    public EmployeeController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/employee")
     public String getEmployee() {
-        return "Name: DileepKumar Tutika, email: dk26092@gmail.com";
+
+        String address = restTemplate.getForObject("http://localhost:8080/address", String.class);
+        return "Name: DileepKumar Tutika, email: dk26092@gmail.com "+ address;
     }
 }
